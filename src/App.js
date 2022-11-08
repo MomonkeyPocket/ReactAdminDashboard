8c6e48ddf734a59f3147b82195ea4f3381af35fa
azkatang <mog2to4aki@gmail.com>
2022-11-08 15:41:21 +0900

    Add Appbar aka Topbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { ColorModeContext, colorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Topbar from './scenes/global/Topbar';
+import Dashboard from './scenes/dashboard';
 
 function App() {
   const [theme, colorMode] = useMode();
-  return (<ColorModeContext.Provider value={colorMode}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="app">
-        <main className='content'></main>
-      </div>
-    </ThemeProvider>
-  </ColorModeContext.Provider>
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className='content'>
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
